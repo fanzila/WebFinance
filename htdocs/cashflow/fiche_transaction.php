b10295e70000fbf5619e734d31ae1725ad895880
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-04-10 12:16:24 +0000

    ajout des champs en update

--- htdocs/cashflow/fiche_transaction.php
+++ htdocs/cashflow/fiche_transaction.php
@@ -1,10 +1,10 @@
-<?php 
-// 
+<?php
+//
 // This file is part of « Webfinance »
 //
 // Copyright (c) 2004-2006 NBI SARL
 // Author : Nicolas Bouthors <nbouthors@nbi.fr>
-// 
+//
 // You can use and redistribute this file under the term of the GNU GPL v2.0
 //
 // $Id$
@@ -12,25 +12,64 @@
 require("../inc/main.php");
 require("../top_popup.php");
 
-$result = mysql_query("SELECT * FROM webfinance_transactions WHERE id=".$_GET['id']) or die(mysql_error());
+$result = mysql_query("SELECT id, id_category, text, amount, type, date, comment FROM webfinance_transactions WHERE id=".$_GET['id'])
+  or die(mysql_error());
 $transaction = mysql_fetch_object($result);
 mysql_free_result($result);
 
 ?>
-
 <form id="main_form" method="post" action="save_transaction.php">
 <input type="hidden" name="id_transaction" value="<?= $transaction->id ?>" />
+<table>
 
-Montant : <input type="text" name="amount" value="<?= $transaction->amount ?>" />
-Catégorie : <select name="id_category"><?php
-$result = mysql_query("SELECT id,name FROM webfinance_categories ORDER BY name");
-while (list($id,$name) = mysql_fetch_array($result)) {
+<tr>
+  <td>Date</td>
+  <td><input type="text" name="date" value="<?=$transaction->date ?>" size="9" /></td>
+</tr>
+<tr>
+  <td>Category</td>
+ <td>
+  <select name="id_category"><?php
+  $result = mysql_query("SELECT id,name FROM webfinance_categories ORDER BY name");
+  while (list($id,$name) = mysql_fetch_array($result)) {
   printf('<option value="%d"%s>%s</option>', $id, ($id==$transaction->id_category)?" selected":"", $name );
-}
+ }
 mysql_free_result($result);
 ?></select>
-
+ </td>
+</tr>
+<tr>
+  <td>Type</td>
+  <td>
+  <select name="type">
+  <option value="real" <?php ("real"==$transaction->type)?" selected":""  ?> >real</option>
+  <option value="prevision" <?php ("prevision"==$transaction->type)?" selected":""  ?> >prevision</option>
+  <option value="asap" <?php ("asap"==$transaction->type)?" selected":""  ?> >asap</option>
+  </select>
+  </td>
+</tr>
+<tr>
+  <td>Description</td>
+  <td><input type="text" size="35" name="text" value="<?=$transaction->text ?>" /></td>
+</tr>
+<tr>
+ <td>Montant :</td>
+ <td>
+  <input type="text" name="amount" value="<?= $transaction->amount ?>" />
+ </td>
+</tr>
+<tr>
+  <td>Comment</td>
+  <td>
+  <textarea rows="3" name="comment"><?=$transaction->comment?></textarea>
+  </td>
+</tr>
+<tr>
+<td colspan="2">
 <input type="submit" value="Enregistrer" />
+</td>
+</tr>
+</table>
 
 
 
--- htdocs/cashflow/save_transaction.php
+++ htdocs/cashflow/save_transaction.php
@@ -1,10 +1,10 @@
-<?php 
-// 
+<?php
+//
 // This file is part of « Webfinance »
 //
 // Copyright (c) 2004-2006 NBI SARL
 // Author : Nicolas Bouthors <nbouthors@nbi.fr>
-// 
+//
 // You can use and redistribute this file under the term of the GNU GPL v2.0
 //
 // $Id$
@@ -13,8 +13,15 @@ require("../inc/main.php");
 
 extract($_POST);
 
-$q = sprintf("UPDATE webfinance_transactions SET id_category=%d WHERE id=%d",
-             $id_category, $id_transaction);
+$q = sprintf("UPDATE webfinance_transactions SET ".
+	     "id_category=%d, ".
+	     "text='%s', ".
+	     "amount=%s, ".
+	     "type='%s', ".
+	     "date='%s', ".
+	     "comment='%s' ".
+	     "WHERE id=%d",
+             $id_category, $text, $amount, $type, $date, $comment, $id_transaction);
 mysql_query($q) or die(mysql_error());
 
 ?>
