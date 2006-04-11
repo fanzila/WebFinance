4baffaee0bd044aa9c86bad65aaef16d02f45c4e
nico <nico@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-04-11 17:32:25 +0000

    - VÃ©rifie la cohÃ©rence du numÃ©ro de compte par rapport Ã  RIB. - FIXME : faire la mÃªme chose sur IBAN => Thierry ? - FIXME2 : Faire ca en JS pour pas avoir Ã  enregistrer pour valider => Nico !

--- htdocs/admin/societe.php
+++ htdocs/admin/societe.php
@@ -72,6 +72,7 @@ Changer le logo <input type="file" name="logo" /> <b>(ONLY PNG)</b>
 <h1>Compte(s) banquaire(s)</h1>
 <table style="text-align: center;" class="framed" cellspacing="0" cellpadding="4">
 <tr class="row_header">
+  <td></td>
   <td>Banque</td>
   <td>Domiciliation</td>
   <td>Code banque</td>
@@ -86,8 +87,49 @@ $result = mysql_query("SELECT id_pref,value FROM webfinance_pref WHERE type_pref
 $count = 1;
 while (list($id_pref,$value) = mysql_fetch_array($result)) {
   $compte = unserialize(base64_decode($value));
+
+  // Check account number. 
+  //
+  // Algorythm is : take the bank code (5 digits) + desk
+  // code (5 digits) + account number (10 digits or letters). You get à 19 char
+  // long "number" (which may contain letters). Replace letters in the following way :
+  // A,J => 1 / B,K,S => 2 / C,L,T => 3 / D,M,U => 4 / E,N,V => 5, F,O,W => 6 /
+  // G,P,X => 7 / H,Q,Y => 8 / I,R,Z => 9. Add 00 to the 19 char number. 
+  //   Checksum number  = 97 - ((21 digit number) % 97)
+  // 
+  // PHP cannot do this calculus with normal functions (number is too big)
+  // MySQL can. So we use a query for that.
+  $bignum = $compte->code_banque.$compte->code_guichet.$compte->compte."00";
+  $bignum = preg_replace("/[AJ]/", "1 ", $bignum);
+  $bignum = preg_replace("/[BKS]/", "2 ", $bignum);
+  $bignum = preg_replace("/[CLT]/", "3 ", $bignum);
+  $bignum = preg_replace("/[DMU]/", "4 ", $bignum);
+  $bignum = preg_replace("/[ENV]/", "5", $bignum);
+  $bignum = preg_replace("/[FOW]/", "6 ", $bignum);
+  $bignum = preg_replace("/[GPX]/", "7 ", $bignum);
+  $bignum = preg_replace("/[HQY]/", "8 ", $bignum);
+  $bignum = preg_replace("/[IRZ]/", "9", $bignum);
+
+  $check_key = mysql_query("SELECT 97 - ($bignum % 97)") or print(mysql_error());
+  list($key) = mysql_fetch_array($check_key);
+  mysql_free_result($check_key);
+
+  if ($key == $compte->clef) {
+    $img = "paid";
+    $hover_text = addslashes(_('The filled account number seems coherent with the check key'));
+  } else {
+    $img = "not_paid";
+    $hover_text = addslashes(_('Checksum fail on account number. Check digits entered.'));
+  }
+  $check_img = sprintf('<img src="/imgs/icons/%s.gif" onmouseover="return escape(\'%s\');" />',
+                       $img, $hover_text );
+                       
+
+  // End check account number
+
   print <<<EOF
 <tr>
+  <td>$check_img</td>
   <td><input style="width: 100px; text-align: center;" type="text" name="banque_$count" value="$compte->banque" /></td>
   <td><input style="width: 100px; text-align: center;" type="text" name="domiciliation_$count" value="$compte->domiciliation" /></td>
   <td><input style="width: 50px; text-align: center;" type="text" name="code_banque_$count" value="$compte->code_banque" /></td>
@@ -102,6 +144,7 @@ EOF;
 }
 ?>
 <tr style="background: #ceffce;">
+  <td></td>
   <td><input style="width: 100px; text-align: center;" type="text" name="banque_new" value="" /></td>
   <td><input style="width: 100px; text-align: center;" type="text" name="domiciliation_new" value="" /></td>
   <td><input style="width: 50px; text-align: center;" type="text" name="code_banque_new" value="" /></td>
