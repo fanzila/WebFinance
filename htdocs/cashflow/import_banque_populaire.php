52b18d88a2072c05b86a3801d20ea89d04a2373c
nico <nico@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-04-13 11:04:12 +0000

    La catÃ©gorie "Unkonwn" DOIT etre un id_category positif le nouveau schema de base cree la categorie nÂ°1 comme "Unknown"

--- htdocs/cashflow/import_banque_populaire.php
+++ htdocs/cashflow/import_banque_populaire.php
@@ -85,7 +85,7 @@ foreach ($transactions as $op) {
 
   // Dans tous les cas on essaie de retrouver la catégorie de la transaction
   // automagiquement.
-  $id_categorie = 0;
+  $id_categorie = 1;
   $result = mysql_query("SELECT COUNT(*),id,name
                          FROM webfinance_categories
                          WHERE re IS NOT NULL
