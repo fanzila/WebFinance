ad7a3f17551e4eb17020f5d7a4d129f7c88c9d82
nico <nico@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-04-16 12:55:30 +0000

    - Suppression de vieilles icones qui n'ont rien Ã  faire lÃ  - Ajout recherche societe.com sur la fiche entreprise

--- htdocs/inc/Client.php
+++ htdocs/inc/Client.php
@@ -21,7 +21,7 @@ class Client {
                                   c.id_client, c.nom,
                                   c.date_created,
                                   c.tel, c.fax,
-                                  c.addr1, c.addr2, c.addr3, c.cp, c.ville, c.pays, c.email,
+                                  c.addr1, c.addr2, c.addr3, c.cp, c.ville, c.pays, c.email, left(cp, 2) as departement,
                                   c.has_devis, c.has_unpaid, c.ca_total_ht, c.ca_total_ht_year, c.total_du_ht,
                                   c.vat_number, c.siren,
                                   c.id_company_type,
@@ -33,6 +33,34 @@ class Client {
       foreach ($data as $n=>$v) { $this->$n = $v; }
       mysql_free_result($result);
     }
+    
+    // If user specified data in the siren field it can be either the RCS number
+    // (format 9 digits) or the INSEE code (format : same 9 digits + 5 digits for
+    // address identifier).
+    // See : http://fr.wikipedia.org/wiki/Codes_INSEE
+
+    // sensible default value
+    $this->link_societe = sprintf('<a href="http://www.societe.com/cgi-bin/liste?nom=%s&dep=%s"><img src="/imgs/icons/societe.com.gif" class="bouton" onmouseover="return escape(\'%s\');" /></a>',
+                                  urlencode($this->nom), $this->departement,
+                                  addslashes( _('Cannot link to societe.com if no RCS or siren specified. Click icon to perform a search.') ) );
+    if ($this->siren != "") {
+      // Trim non-digits from value
+      $this->siren = preg_replace("/[^0-9]/", "", $this->siren);
+      switch (strlen($this->siren)) {
+        case 9: // RCS
+          $this->link_societe = sprintf('<a href="http://www.societe.com/cgi-bin/recherche?rncs=%s"><img src="/imgs/icons/societe.com.gif" class="bouton" onmouseover="return escape(\'%s\');" /></a>',
+                                        $this->siren, addslashes( _('See financial info about this company on Societe.com') ) 
+                                       );
+          $this->siren = preg_replace("!([0-9]{3})([0-9]{3})([0-9]{3})!", '\\1 \\2 \\3', $this->siren);
+          break;
+        case 14: // INSEE
+          $this->link_societe = sprintf('<a href="http://www.societe.com/cgi-bin/recherche?rncs=%s"><img src="/imgs/icons/societe.com.gif" class="bouton" onmouseover="return escape(\'%s\');" /></a>',
+                                        substr($this->siren, 0, 9), addslashes( _('See financial info about this company on Societe.com') ) 
+                                       );
+          $this->siren = preg_replace("!([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{5})!", '\\1 \\2 \\3 \\4', $this->siren);
+          break;
+      }
+    } 
   }
 
   function Client($id = null) {
--- htdocs/prospection/fiche_prospect_interne.php
+++ htdocs/prospection/fiche_prospect_interne.php
@@ -7,16 +7,20 @@
 // 
 // You can use and redistribute this file under the term of the GNU GPL v2.0
 //
+
+global $Client;
+
+
 ?>
   <b>Social :</b><br/>
-  <input type="text" class="tva" name="vat_number" value="<?= $Client->data->vat_number ?>" class="vat_number" /><br>
-  <input type="text" class="siren" name="siren" value="<?= $Client->data->siren ?>" class="siren" /><br>
+  <input type="text" class="tva" name="vat_number" value="<?= $Client->vat_number ?>" class="vat_number" /><br>
+  <input type="text" class="siren" name="siren" value="<?= $Client->siren ?>" class="siren" />&nbsp;<?= $Client->link_societe ?><br>
   <?// Interne  ?>
   <b>Interne : </b><br/>
   <select style="font-size: 10px; width: 200px;" name="id_company_type"><?php
   $result = mysql_query("SELECT id_company_type,nom FROM webfinance_company_types ORDER BY nom");
   while ($t = mysql_fetch_object($result)) {
-    printf('<option value="%s" %s>%s</option>'."\n", $t->id_company_type, ($Client->data->id_company_type == $t->id_company_type)?"selected":"", ucfirst($t->nom));
+    printf('<option value="%s" %s>%s</option>'."\n", $t->id_company_type, ($Client->id_company_type == $t->id_company_type)?"selected":"", ucfirst($t->nom));
   }
   ?>
   </select><br/>
