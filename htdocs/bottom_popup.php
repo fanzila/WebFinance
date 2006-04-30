288775cab595ca975a06c5620a4364ccbd270d0c
nico <nico@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-04-30 11:04:37 +0000

    Bugfix

--- htdocs/bottom_popup.php
+++ htdocs/bottom_popup.php
@@ -8,6 +8,7 @@
 // You can use and redistribute this file under the term of the GNU GPL v2.0
 //
 ?>
+<script type="text/javascript" language=javascript src=/js/inpage_popup.js></script>
 <script type="text/javascript" language=javascript src=/js/wz_tooltip.js></script>
 </body>
 
