a1978559a6df0e761ade7d1601082d3e29a6f3cf
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-06-10 07:43:47 +0000

    déplacer les roles dans les préferences

--- wbfinance/htdocs/admin/index.php
+++ wbfinance/htdocs/admin/index.php
@@ -23,13 +23,6 @@ function confirmDeleteUser(id) {
     window.location = 'save_user.php?action=delete&id='+id;
   }
 }
-function confirmDeleteRole(id) {
-  if (confirm('Voulez-vous vraiment supprimer cet role ?')) {
-    window.location = 'save_roles.php?action=delete&id='+id;
-  }
-}
-
-
 </script>
 
 <?= $_SESSION['message']; $_SESSION['message']=""; ?>
@@ -72,48 +65,6 @@ $help_rights=addslashes("Les valeurs possible sont: client,manager,accounting,em
 ?>
 </table><br/>
 <a href="fiche_user.php?id=-1"><?= _('Créer un utilisateur') ?></a>
-
-<p>
-<h2>Roles</h2>
-
-<form action="save_roles.php" id="main_form" method="post">
-
-<table border="0" cellspacing="0" cellpadding="3" class="framed">
-<tr style="text-align: center;" class="row_header">
-  <td><?= _('Name') ?><img class="help_icon" src="/imgs/icons/help.png" onmouseover="return escape('<?= $help_rights ?>');" /> </td>
-  <td><?= _('Description') ?></td>
-  <td><?= _('Actions') ?></td>
-</tr>
-<?php
-
-$result = mysql_query("SELECT id_role, name, description
-                       FROM webfinance_roles
-                       ORDER BY name") or wf_mysqldie();
-while ($c = mysql_fetch_assoc($result)) {
-  extract($c);
-
-  print <<<EOF
-<tr class="row_even">
-  <td><input type="text" name="cat[$id_role][name]" value="$name" style="width: 80px;" /></td>
-  <td><input type="text" name="cat[$id_role][description]" value="$description" style="width: 200px;" /></td>
-  <td align="center"><a href="javascript:confirmDeleteRole($id_role);"><img src="/imgs/icons/delete.gif" /></a>
-</tr>
-EOF;
-}
-
-?>
-<tr style="background: #ceffce;">
-  <td><input type="text" name="cat[new][name]" value="" style="width: 80px;" /></td>
-  <td><input type="text" name="cat[new][description]" value="" style="width: 200px;" /></td>
-  <td></td>
-</tr>
-<tr class="row_even">
-  <td style="text-align: center;" colspan="3"><input type="submit" value="<?= _('Save') ?>" /></td>
-</table>
-
-</form>
-
-</p>
 
 
 <?php
