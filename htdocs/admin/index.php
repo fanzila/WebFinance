4e9795e9fbb90f5a50d10241b0cfecbef6e7b281
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-06-14 15:08:46 +0000

    tri par colonne

--- htdocs/admin/index.php
+++ htdocs/admin/index.php
@@ -27,19 +27,47 @@ function confirmDeleteUser(id) {
 
 <?= $_SESSION['message']; $_SESSION['message']=""; ?>
 
-<h2>Utilisateurs backoffice</h2>
+<h2><?=_('BackOffice users')?></h2>
+<?
+  $order = "DESC";
+  $link = "&d=1";
+  if(isset($_GET['d']) AND $_GET['d']==1){
+    $order = "";
+    $link = "";
+  }
+
+?>
+
 
 <table border="0" cellspacing="0" cellpadding="5" class="framed">
 <tr align=center class="row_header">
-  <td>Login</td>
-  <td>Nom</td>
-  <td>Mail</td>
-  <td>Last login</td>
+  <td><a href="?sort=login<?=$link?>">Login</a></td>
+  <td><a href="?sort=name<?=$link?>">Nom</a></td>
+  <td><a href="?sort=email<?=$link?>">Mail</a></td>
+  <td><a href="?sort=last_login<?=$link?>">Last login</a></td>
   <td>Actions</td>
 </tr>
 <?php
+  $critere = " last_login $order ";
+  if(isset($_GET['sort'])){
+    switch ($_GET['sort']) {
+    case "login" :
+       $critere = " login $order ";
+       break;
+    case "name" :
+      $critere = " first_name $order, last_name $order ";
+      break;
+    case "email" :
+      $critere = " email $order ";
+      break;
+    case "last_login" :
+      $critere = " last_login $order";
+      break;
+    }
+  }
+
 $result = mysql_query("SELECT first_name,last_name,id_user,email,login, role, date_format(last_login,'%d/%m/%Y') as nice_last_login
-                       FROM webfinance_users ORDER by last_login DESC");
+                       FROM webfinance_users ORDER by ".$critere) or wf_mysqldie();
 $count=1;
 while ($user = mysql_fetch_object($result)) {
   $rowclass = ($count%2)==0?"odd":"even";
@@ -52,7 +80,7 @@ while ($user = mysql_fetch_object($result)) {
   <td>$user->nice_last_login</td>
   <td>
     <a href="javascript:confirmDeleteUser($user->id_user);"><img src="/imgs/icons/delete.png" alt="<?= _('Delete')?>" /></a>
-    <a href="#" onclick="inpagePopup(event, this, 280, 260, 'edit_user.php?id=$user->id_user');" ><img src="/imgs/icons/edit.png" alt="Modifier" /></a>
+    <a href="#" onclick="inpagePopup(event, this, 280, 260, 'edit_user.php?id=$user->id_user$link');" ><img src="/imgs/icons/edit.png" alt="Modifier" /></a>
   </td>
 </tr>
 EOF;
@@ -64,7 +92,7 @@ mysql_free_result($result);
 
 ?>
 </table><br/>
-<a href="#" onclick="inpagePopup(event, this, 280, 260, 'edit_user.php?id=-1');"><?= _('Add a user') ?></a>
+<a href="#" onclick="inpagePopup(event, this, 280, 260, 'edit_user.php?id=-1<?=$link?>');"><?= _('Add a user') ?></a>
 
 <?php
 
