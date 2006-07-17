fab39fcb1a53e3ad45218d7475396086d25a935f
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-07-17 15:09:40 +0000

    ajouter les paramètres de Paybox dans les préférences

---
+++ wbfinance/htdocs/admin/save_paybox.php
@@ -0,0 +1,30 @@
+<?php
+//
+// This file is part of « Webfinance »
+//
+// Copyright (c) 2004-2006 NBI SARL
+// Author : Nicolas Bouthors <nbouthors@nbi.fr>
+//
+// You can use and redistribute this file under the term of the GNU GPL v2.0
+//
+
+// $Id$
+
+require("../inc/main.php");
+
+  $data = new stdClass();
+  $data->PBX_SITE = $_GET['PBX_SITE'];
+  $data->PBX_RANG = $_GET['PBX_RANG'];
+  $data->PBX_IDENTIFIANT = $_GET['PBX_IDENTIFIANT'];
+  $data = base64_encode(serialize($data));
+
+if($_GET['id']>0)
+  mysql_query("UPDATE webfinance_pref SET value='$data' WHERE id_pref=".$_GET['id']) or wf_mysqldie();
+ else
+   mysql_query("INSERT INTO webfinance_pref SET type_pref='paybox', value='$data' ")
+     or wf_mysqldie();
+
+header("Location: preferences.php?tab=Paybox");
+
+
+?>
