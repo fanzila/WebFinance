7ced748aa08b2bbc9677c98f5ae74dcba6633b08
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-08-18 11:22:50 +0000

    ajout des debug de OSH

--- htdocs/bottom.php
+++ htdocs/bottom.php
@@ -27,7 +27,29 @@ if($_SESSION['debug']==1){
   echo '<b>$_SERVER:</b>';
   print_r($_SERVER);
 
+  $microsecondes=microtime();
+  list($micro,$time)=explode(' ',$microsecondes);
+  $mt_end=$micro+$time;
+
+  echo "<b>Time elapsed</b>: ".round(($mt_end-$mt_start)*1000)." ms \n";
+  echo "<b>Query number</b>: $query_number\n";
+
+  echo "<b>mysql stats:</b>\n";
+  $status = explode('  ', mysql_stat());
+  print_r($status);
+
+  echo "<b>mysql processes</b>:\n";
+  $result = mysql_list_processes();
+  while ($row = mysql_fetch_assoc($result)){
+    printf("%s %s %s %s %s\n", $row["Id"], $row["Host"], $row["db"],
+	   $row["Command"], $row["Time"]);
+  }
+  mysql_free_result($result);
+  echo "<b>Locales:</b>\n";
+  system("locale");
+
   echo '</pre></div>';
+
 }
 ?>
 <div id="revision"><?= $Revision ?></div>
