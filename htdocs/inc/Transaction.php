6cdf162cc168c9e86b70b7f262a90a3304175363
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-08-23 09:03:15 +0000

    clean

--- htdocs/inc/Transaction.php
+++ htdocs/inc/Transaction.php
@@ -10,10 +10,10 @@
 // $Id$
 //
 /// Transaction.php
-/// 
+///
 /// Transaction object aims to manage a transaction (operation in a bank
 /// account) namely
-// 
+//
 
 class Transaction extends WFO {
   var $id = -1;
@@ -24,15 +24,17 @@ class Transaction extends WFO {
 
   function _getInfos() {
     $result = $this->SQL("SELECT id_account, id_category, text, amount, type, document, date, date_update, comment, lettrage, id_invoice
-                           FROM webfinance_transactions 
+                           FROM webfinance_transactions
                            WHERE id=".$this->id);
     if (mysql_num_rows($result)) {
       $data = mysql_fetch_assoc($result);
-      foreach ($data as $n=>$v) { $this->$n = $v; }
+      foreach ($data as $n=>$v) {
+	$this->$n = $v;
+      }
       mysql_free_result($result);
     }
   }
-    
+
   function Transaction($id = null) {
     if (is_numeric($id)) {
       $this->id = $id;
@@ -53,12 +55,12 @@ class Transaction extends WFO {
       The idea of this method is mainly to help the importation of bank
       transaction from banking internet sites. We are given an amount and a
       text for a transaction and we try to find the invoices that are paid by
-      this transaction. 
-      
-      This might be tricky : 
+      this transaction.
+
+      This might be tricky :
         1) invoices with the same amount are not uncommon
         2) some clients pay several invoices with the same transfer or check
-        3) checks can be sent to the bank by pack of 2 or more 
+        3) checks can be sent to the bank by pack of 2 or more
       So we must not assume the amount of the transaction will match closely
       with the invoice. It can also match for two or more invoices.
 
@@ -70,28 +72,30 @@ class Transaction extends WFO {
 
   */
   function findRelatedInvoice() {
-    if ($this->amount == null) { return array(); }
+    if ($this->amount == null) {
+      return array();
+    }
 
     $matches = array();
     $this->_getInfos(); // Populate $this if empty
 
-    /** 
+    /**
     First the simple case : user has already explicitly linked this transaction
     with an invoice. We just return that invoice id. Over
     */
     if ($this->id_invoice) {
       array_push($matches, $this->id_invoice);
       return $matches;
-    } 
-    
+    }
+
     /**
      Now whe have to guess or find the invoice(s)... try to be clever.
-    
+
      1) Common case : client pays one invoice by bank transfer. Text of imported transaction
         contains the name of the client and amount matches only one invoice. We
         search unpaid invoices where client name matches the transaction
         text.
-    
+
         Societe Générale (and maybe some others) truncate texts comming from
         other banks to 19 chars so the match on the name is done on the same
         length. FIXME maybe there is a more elegant way  to do this trimming
@@ -99,11 +103,11 @@ class Transaction extends WFO {
     */
     $result = $this->SQL("SELECT fl.id_facture,1.196*sum(fl.qtt*fl.prix_ht) AS total, CONCAT('%', left(c.nom,19), '%') as re_clientname, c.nom
                            FROM webfinance_invoice_rows AS fl, webfinance_invoices AS f, webfinance_clients AS c
-                           WHERE c.id_client=f.id_client 
+                           WHERE c.id_client=f.id_client
                            AND f.type_doc = 'facture'
                            AND f.is_paye = 0
                            AND f.id_facture=fl.id_facture
-                           GROUP BY f.id_facture 
+                           GROUP BY f.id_facture
                            HAVING '".$this->text."' LIKE re_clientname AND total=".$this->amount);
     if (mysql_num_rows($result) == 1) {
       // Only one it must be THE one return it
@@ -127,7 +131,7 @@ class Transaction extends WFO {
     */
     $result = $this->SQL("SELECT count(*), CONCAT('%', left(c.nom,10), '%') as re_clientname, c.nom
                            FROM webfinance_invoice_rows AS fl, webfinance_invoices AS f, webfinance_clients AS c
-                           WHERE c.id_client=f.id_client 
+                           WHERE c.id_client=f.id_client
                            AND f.type_doc = 'facture'
                            AND f.is_paye = 0
                            AND f.id_facture=fl.id_facture
@@ -139,11 +143,11 @@ class Transaction extends WFO {
       $result = $this->SQL("SELECT SUM(total),id_client FROM (
                                SELECT fl.id_facture,1.196*sum(fl.qtt*fl.prix_ht) AS total, CONCAT('%', left(c.nom,10), '%') as re_clientname, c.nom, c.id_client
                                FROM webfinance_invoice_rows AS fl, webfinance_invoices AS f, webfinance_clients AS c
-                               WHERE c.id_client=f.id_client 
+                               WHERE c.id_client=f.id_client
                                AND f.type_doc = 'facture'
                                AND f.is_paye = 0
                                AND f.id_facture=fl.id_facture
-                               GROUP BY f.id_facture 
+                               GROUP BY f.id_facture
                                HAVING '".$this->text."' LIKE re_clientname
                                ORDER BY f.date_facture DESC LIMIT $i
                              ) AS t GROUP BY id_client");
@@ -156,7 +160,7 @@ class Transaction extends WFO {
         // very good that this transaction pays for this $i invoices. Now we
         // have to find back the id of this invoices (the SUM in last query
         // makes it impossible to have IDs AND total amount)
-        $result = $this->SQL("SELECT f.id_facture 
+        $result = $this->SQL("SELECT f.id_facture
                                FROM webfinance_invoices AS f
                                WHERE f.id_client=$id_client
                                ORDER BY f.date_facture DESC LIMIT $i");
@@ -168,7 +172,7 @@ class Transaction extends WFO {
       }
     }
 
-    /** 
+    /**
       3) Another common case : Several clients pays their invoices by check. We
          send those check all at once to the bank. The transaction apearing in
          the account only shows the total of all checks. Try to find the
@@ -176,22 +180,22 @@ class Transaction extends WFO {
          restrict our search to one client : we do not match on the text of the
          transaction (FIXME : Might also be a match like '.*Remise de
          cheques.*' would work for me)
-     */ 
+     */
 
     // FIXME : copy paste and adapt above logic
 
-    return $matches; 
-  }
-
-  /// Returns an array containing the id of possible matching categories
-  function findRelatedCategory() {
+    return $matches;
   }
 
   function save() {
     // print_r($this);
     if ($this->id == -1) {
-      if ($this->text == "") { die("Transaction::save Cannot add a transaction without a text"); }
-      if ($this->date == "") { $this->date = strftime("%Y-%m-%d"); }
+      if ($this->text == "") {
+	die("Transaction::save Cannot add a transaction without a text");
+      }
+      if ($this->date == "") {
+	$this->date = strftime("%Y-%m-%d");
+      }
       $q = sprintf("INSERT INTO webfinance_transactions (id_account, id_category, text, amount, type, document, date, comment, lettrage, id_invoice)
                                                   VALUES(%d,         %d,          '%s', '%s',   '%s', '%s',     '%s', '%s',    %d,       %d)",
                     $this->id_account, $this->id_category, $this->text, $this->amount, $this->type, $this->document, $this->date, $this->comment, $this->lettrage, $this->id_invoice );
