19440f2bfddd2ec44fdf4915630dd01401762d8c
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2006-09-26 12:56:36 +0000

    on n'affiche pas au comptable les categories, l'import, paybox

--- htdocs/cashflow/nav.php
+++ htdocs/cashflow/nav.php
@@ -17,10 +17,10 @@ $elements = array(
                   _('Transactions') => array('url'=>'./', 'roles'=>'manager,accounting'),
                   _('Graphics') => array('url'=>'graphs.php', 'roles'=>'manager,accounting'),
                   _('Graphics All') => array('url'=>'graphs_all_history.php', 'roles'=>'manager,accounting'),
-                  _('Categories') => array('url' => 'categories.php', 'roles' => 'manager,accounting'),
+                  _('Categories') => array('url' => 'categories.php', 'roles' => 'manager'),
                   _('Expenses') => array('url'=>'expenses.php', 'roles'=>'manager,accounting,employee'),
-                  _('Import') => array('url'=>'import.php', 'roles'=>'manager,accounting'),
-                  _('Paybox') => array('url'=>'paybox.php', 'roles'=>'manager,accounting,employee'),
+                  _('Import') => array('url'=>'import.php', 'roles'=>'manager'),
+                  _('Paybox') => array('url'=>'paybox.php', 'roles'=>'manager,employee'),
                  );
 
 
