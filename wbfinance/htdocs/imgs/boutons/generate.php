6b6fced5d543e00fde8f8b1b6b281823beb31ad1
cyb <cyb@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2007-04-18 14:14:57 +0000

    - default theme to 'main' - clean

--- wbfinance/htdocs/imgs/boutons/generate.php
+++ wbfinance/htdocs/imgs/boutons/generate.php
@@ -8,7 +8,7 @@ if (!isset($_GET['data'])) {
 
 $font_size = 8;
 
-$theme = $User->prefs->theme;
+$theme = (isset($User->prefs->theme) ? isset($User->prefs->theme) : 'main');
 $data = base64_decode($_GET['data']);
 list($text, $style) = explode(":", $data);
 
@@ -23,11 +23,11 @@ if (!file_exists("../../css/themes/$theme/buttonfont.ttf")) {
 $font = "../../css/themes/$theme/buttonfont.ttf";
 
 // Load the background PNG
-$img = imagecreatefrompng($base_image); 
-// Allocate black 
+$img = imagecreatefrompng($base_image);
+// Allocate black
 $black = imagecolorallocate($img, 255, 0, 0, 127);
 // Blends the font into the background
-imagealphablending($img, TRUE); 
+imagealphablending($img, TRUE);
 
 list($width, $height, $ignored1, $ignored2) = getimagesize($base_image);
 $bounding_box = imageftbbox($font_size, 0, $font, $text);
@@ -38,7 +38,7 @@ $text_height  = abs($bounding_box[1] - $bounding_box[5]);
 $text_x = ($width - $text_width) / 2;
 $text_y = $height - (3 + ($height - $text_height) / 2);
 
-imagettftext($img, $font_size, 0, $text_x, $text_y, $black, $font, $text); 
+imagettftext($img, $font_size, 0, $text_x, $text_y, $black, $font, $text);
 
 header("Content-Type: image/png");
 imagepng($img);
