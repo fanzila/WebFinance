9e02bbec626e31f152c55294f67c889e6444d5e3
cyb <cyb@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2007-04-19 08:57:09 +0000

    better icons support (more portable, will be re-used in PHPLinker)

--- wbfinance/htdocs/404.php
+++ wbfinance/htdocs/404.php
@@ -1,10 +1,10 @@
-<?php 
-// 
+<?php
+//
 // This file is part of Â« Webfinance Â»
 //
 // Copyright (c) 2004-2006 NBI SARL
 // Author : Nicolas Bouthors <nbouthors@nbi.fr>
-// 
+//
 // You can use and redistribute this file under the term of the GNU GPL v2.0
 //
 ?>
@@ -15,9 +15,9 @@ include("inc/main.php");
 if (($GLOBALS['HTTP_SERVER_VARS']['REDIRECT_STATUS'] == "404") && (preg_match("/.html$/", $GLOBALS['HTTP_SERVER_VARS']['REDIRECT_URL']))) {
   $new_loc = preg_replace("/(\w+)\.html/", "index.php?file=\\1.html", $GLOBALS['HTTP_SERVER_VARS']['REDIRECT_URL']);
   header("Location: $new_loc");
-} elseif (($GLOBALS['HTTP_SERVER_VARS']['REDIRECT_STATUS'] == "404") && (preg_match("!/imgs/boutons/([^\.]+).png$!", $GLOBALS['HTTP_SERVER_VARS']['REDIRECT_URL'], $matches))) {
+} elseif (($GLOBALS['HTTP_SERVER_VARS']['REDIRECT_STATUS'] == "404") && (preg_match("!/imgs/boutons/([^\.]+)_([^\.]+)_([^\.]+).png$!", $GLOBALS['HTTP_SERVER_VARS']['REDIRECT_URL'], $matches))) {
   // Génération des images dynamiquement.
-  header("Location: /imgs/boutons/generate.php?data=".urlencode($matches[1]));
+  header("Location: /imgs/boutons/generate.php?text=".$matches[1].'&style='.$matches[2].'&theme='.$matches[3]);
   die();
 } else {
   require("top.php");
--- wbfinance/htdocs/imgs/boutons/generate.php
+++ wbfinance/htdocs/imgs/boutons/generate.php
@@ -2,30 +2,28 @@
 
 include("../../inc/main.php");
 
-if (!isset($_GET['data'])) {
-  die();
+if (!isset($_GET['text'], $_GET['style'], $_GET['theme'])) {
+  die('missing argument');
 }
 
 $font_size = 8;
 
-$theme = (isset($User->prefs->theme) ? $User->prefs->theme : 'main');
-$data = base64_decode($_GET['data']);
-list($text, $style) = explode(":", $data);
+$filename=$_GET['text'].'_'.$_GET['style'].'_'.$_GET['theme'].'.png';
 
-if(file_exists($_GET['data'].'.png')) {
-  header('Location: ./'. $_GET['data'].'.png');
+if(file_exists($filename)) {
+  header("Location: ./$filename");
   exit;
 }
 
-if (!file_exists("../../css/themes/$theme/$style.png")) {
-  die("No base image '$style.png' for this theme !");
+$base_image = '../../css/themes/'.$_GET['theme'].'/'.$_GET['style'].'.png';
+if (!file_exists($base_image)) {
+  die("No base image '$base_image' for this theme !");
 }
-$base_image = "../../css/themes/$theme/$style.png";
 
-if (!file_exists("../../css/themes/$theme/buttonfont.ttf")) {
-  die("No font for this theme");
+$font = '../../css/themes/'.$_GET['theme'].'/buttonfont.ttf';
+if (!file_exists($font)) {
+  die("No font for this theme ($font)");
 }
-$font = "../../css/themes/$theme/buttonfont.ttf";
 
 // Load the background PNG
 $img = imagecreatefrompng($base_image);
@@ -35,7 +33,7 @@ $black = imagecolorallocate($img, 255, 0, 0, 127);
 imagealphablending($img, TRUE);
 
 list($width, $height, $ignored1, $ignored2) = getimagesize($base_image);
-$bounding_box = imageftbbox($font_size, 0, $font, $text);
+$bounding_box = imageftbbox($font_size, 0, $font, $_GET['text']);
 
 $text_width = abs($bounding_box[0] - $bounding_box[4]);
 $text_height  = abs($bounding_box[1] - $bounding_box[5]);
@@ -43,10 +41,10 @@ $text_height  = abs($bounding_box[1] - $bounding_box[5]);
 $text_x = ($width - $text_width) / 2;
 $text_y = $height - (3 + ($height - $text_height) / 2);
 
-imagettftext($img, $font_size, 0, $text_x, $text_y, $black, $font, $text);
+imagettftext($img, $font_size, 0, $text_x, $text_y, $black, $font, $_GET['text']);
 
-header("Content-Type: image/png");
-imagepng($img);
-imagepng($img, $_GET['data'].".png");
+imagepng($img, $filename);
+
+header("Location: ./$filename");
 
 ?>
