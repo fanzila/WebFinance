61c28327d19bc0b8205ca7d2cb8444a26033e2c0
thierry <thierry@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2007-06-14 10:39:19 +0000

    added GPL header

--- htdocs/nav.php
+++ htdocs/nav.php
@@ -1,11 +1,13 @@
 <?php
 /*
+ Copyright (C) 2004-2006 NBI SARL, ISVTEC SARL
+
    This file is part of Webfinance.
 
-    Webfinance is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   Webfinance is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
     Webfinance is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
