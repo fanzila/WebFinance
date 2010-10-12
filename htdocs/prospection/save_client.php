964ac369f81a253a63235de9a5e7a3e1fb6ef3db
cyb <cyb@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2010-10-12 08:34:48 +0000

    removed stupid duplicated copyright

--- htdocs/prospection/save_client.php
+++ htdocs/prospection/save_client.php
@@ -18,18 +18,6 @@
     along with Webfinance; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?>
-<?php
-//
-// This file is part of « Webfinance »
-//
-// Copyright (c) 2004-2006 NBI SARL
-// Author : Nicolas Bouthors <nbouthors@nbi.fr>
-//
-// You can use and redistribute this file under the term of the GNU GPL v2.0
-//
-?>
-<?php
 
 require("../inc/main.php");
 must_login();
