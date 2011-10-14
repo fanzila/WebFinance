670fbf13ab917c2f5e55450ef3fee8b96a8fac0c
cyb <cyb@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2011-10-14 23:55:56 +0000

    Finished template migration

---
+++ htdocs/inc/smarty.php
@@ -0,0 +1,28 @@
+<?php
+//
+// Copyright (C) 2011 Cyril Bouthors <cyril@bouthors.org>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+require('smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty_home = dirname(__FILE__) . '/../../template/';
+
+$smarty->template_dir = $smarty_home .'/template';
+$smarty->compile_dir = $smarty->config_dir = $smarty->cache_dir
+  = $smarty_home . '/cache';
+
+?>
