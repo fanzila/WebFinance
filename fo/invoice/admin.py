93dec0efce7cf348c4ab914309bf77cd895dc6b7
wilane <wilane@265bc5b2-bbc9-4297-9d4f-9469996690ef>
2011-11-10 18:28:30 +0000

    Initial models without any code, this is just a dbinspect follwed by some editing. The admin can be seen running on http://webfinance.wilane.org/fo/admin  admin/admin

---
+++ fo/invoice/admin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#$Id$
+
+__author__ = "Ousmane Wilane ♟ <ousmane@wilane.org>"
+__date__   = "Thu Nov 10 15:57:28 2011"
+
+
+from fo.libs.admin import autoregister
+
+autoregister('invoice')
---
+++ fo/local_settings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#$Id$
+
+__author__ = "Ousmane Wilane ♟ <ousmane@wilane.org>"
+__date__   = "Thu Nov 10 13:25:50 2011"
+
+
+ADMINS = (
+    ('Cyril Bouthors', 'cyril.bouthors@isvtec.com'),
+    ('Ousmane Wilane', 'ousmane@wilane.org'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webfinance',
+        'USER': 'root',
+        #'PASSWORD': 'lptevtg',
+        'HOST': '10.42.0.1', 
+        'PORT': '',
+    }
+}
