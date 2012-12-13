399a7eb5c01b2baaed52937b550f72d18fbb9e00
Pierre Doleans <pierre@doleans.net>
2012-12-13 21:00:56 -0100

    Improve reporting

--- htdocs/prospection/reporting.php
+++ htdocs/prospection/reporting.php
@@ -24,6 +24,8 @@ $roles = 'manager,employee';
 include("../top.php");
 include("nav.php");
 
+if(isset($_GET['disp_req'])) $disp_req = true;
+
 function mysqlQuery($q) {
 	$r = mysql_query($q) or die("QUERY ERROR: $q ".mysql_error());
 	return mysql_fetch_object($r);
@@ -149,8 +151,66 @@ $req5c = "SELECT id_client, nom, date_created
 	FROM webfinance_clients AS c  
 	WHERE YEAR(c.date_created) = $year";
 	
+$req6 = "SELECT count(*) AS total
+		FROM webfinance_invoices AS f 
+		LEFT JOIN webfinance_invoice_rows AS r ON f.id_facture = r.id_facture 
+		LEFT JOIN webfinance_clients AS c ON f.id_client = c.id_client 
+		WHERE r.description LIKE '%GTR%' 
+		AND r.description NOT LIKE '%Pas de GTR%' 
+		AND qtt > 0 
+		AND f.id_client IN (SELECT id_facture 
+			FROM webfinance_invoices 
+			WHERE f.period <> 'none' 
+			AND f.type_doc = 'facture')";
+
+$req6_gtr4 = "SELECT count(*) AS total
+			FROM webfinance_invoices AS f 
+			LEFT JOIN webfinance_invoice_rows AS r ON f.id_facture = r.id_facture 
+			LEFT JOIN webfinance_clients AS c ON f.id_client = c.id_client 
+			WHERE r.description LIKE '%GTR%' 
+			AND description LIKE '%bureau%' 
+			AND r.description NOT LIKE '%Pas de GTR%' 
+			AND qtt > 0 
+			AND f.id_client IN (SELECT id_facture 
+				FROM webfinance_invoices 
+				WHERE f.period <> 'none' 
+				AND f.type_doc = 'facture')";
+
+$req6_gtr4_gold = "SELECT count(*) AS total
+			FROM webfinance_invoices AS f 
+			LEFT JOIN webfinance_invoice_rows AS r ON f.id_facture = r.id_facture 
+			LEFT JOIN webfinance_clients AS c ON f.id_client = c.id_client 
+			WHERE r.description LIKE '%GTR Gold%' 
+			AND qtt > 0 
+			AND f.id_client IN (SELECT id_facture 
+				FROM webfinance_invoices 
+				WHERE f.period <> 'none' 
+				AND f.type_doc = 'facture')";
+
+$req6_gtr4_prems = "SELECT count(*) AS total
+			FROM webfinance_invoices AS f 
+			LEFT JOIN webfinance_invoice_rows AS r ON f.id_facture = r.id_facture 
+			LEFT JOIN webfinance_clients AS c ON f.id_client = c.id_client 
+			WHERE r.description LIKE '%GTR Premium%' 
+			AND qtt > 0 
+			AND f.id_client IN (SELECT id_facture 
+				FROM webfinance_invoices 
+				WHERE f.period <> 'none' 
+				AND f.type_doc = 'facture')";
+
+$req6_gtr4_plus1 = "SELECT count(*) AS total
+			FROM webfinance_invoices AS f 
+			LEFT JOIN webfinance_invoice_rows AS r ON f.id_facture = r.id_facture 
+			LEFT JOIN webfinance_clients AS c ON f.id_client = c.id_client 
+			WHERE r.description LIKE '%GTR h+1%' 
+			AND qtt > 0 
+			AND f.id_client IN (SELECT id_facture 
+				FROM webfinance_invoices 
+				WHERE f.period <> 'none' 
+				AND f.type_doc = 'facture')";
+							
 if(isset($_GET['popup'])) { 
-	
+
 $Facture = new Facture();
 
 ?>	
@@ -203,17 +263,28 @@ $Facture = new Facture();
 	exit;
 }
 
-$total4 = mysqlQuery($req4)->total;
-$total4_ponctu = mysqlQuery($req4a)->total;
-$recu4 = $total4 - $total4_ponctu;
-$perc_recu4 = round(($recu4*100)/$total4);
-$perc_ponctu4 = round(($total4_ponctu*100)/$total4);
+$total4 			= mysqlQuery($req4)->total;
+$total4_ponctu		= mysqlQuery($req4a)->total;
+$recu4 				= $total4 - $total4_ponctu;
+$perc_recu4 		= round(($recu4*100)/$total4);
+$perc_ponctu4 		= round(($total4_ponctu*100)/$total4);
 
-$total5 = mysqlQuery($req5)->total;
-$total5_ponctu = mysqlQuery($req5a)->total;
-$recu5 = $total5 - $total5_ponctu;
-$perc_recu5 = round(($recu5*100)/$total5);
-$perc_ponctu5 = round(($total5_ponctu*100)/$total5);
+$total5 			= mysqlQuery($req5)->total;
+$total5_ponctu 		= mysqlQuery($req5a)->total;
+$recu5	 			= $total5 - $total5_ponctu;
+$perc_recu5			= round(($recu5*100)/$total5);
+$perc_ponctu5 		= round(($total5_ponctu*100)/$total5);
+
+$total6 			= mysqlQuery($req6)->total;
+$total6_gtr 		= mysqlQuery($req6_gtr4)->total;
+$total6_gtr_per 	= ($total6_gtr*100)/$total6;
+$total6_gtr4g		= mysqlQuery($req6_gtr4_gold)->total;
+$total6_gtr4g_per	= ($total6_gtr4g*100)/$total6;
+$total6_gtrprem 	= mysqlQuery($req6_gtr4_prems)->total;
+$total6_gtrprem_per	= ($total6_gtrprem*100)/$total6;
+$total6_plus1		= mysqlQuery($req6_gtr4_plus1)->total;
+$total6_plus1_per	= round(($total6_plus1*100)/$total6,2);
+
 ?>
 <br />
 <form onsubmit="">
@@ -226,61 +297,40 @@ $perc_ponctu5 = round(($total5_ponctu*100)/$total5);
 <?php for ($lyear = 2006 ; $lyear <= 2020 ; $lyear++) { ?>
 	<option value="<?php echo $lyear ?>" <? if($lyear == $year) echo 'selected' ?>><?php echo $lyear; ?></option>
 	<?php } ?>	
-</select> <input style="width: 50px; height: 20px;" class="bordered" type="submit" name="send" value="Display"> <input type="checkbox" name="req" value="1"> (show queries)
+</select> <input style="width: 50px; height: 20px;" class="bordered" type="submit" name="send" value="Display"> <input type="checkbox" name="disp_req" value="1"> (show queries)
 </form>
 <br />
 <br />
 <table border="0" cellspacing="0" cellpadding="10" class="framed">
 	<tr class="row_header" style="text-align: center;"><td colspan="3">Reporting</td></tr>
 	<tr class="misc1">
-		<td><!-- REQ1 -->1. CA par mois HT Facturé</th><td><?=numberFormat(mysqlQuery($req1)->total)?> €</td><td>CA par mois HT Facturé</td>
+		<td><!-- REQ1 -->1. CA par mois HT Facturé</td><td><?=numberFormat(mysqlQuery($req1)->total)?> €</td><td>CA par mois HT Facturé</td>
 	</tr>
 	<tr class="misc2">
-		<td><!-- REQ2 -->2. CA sur opportunités gagnées</th><td><?=numberFormat(mysqlQuery($req2)->total)?>  €</td><td>CA HT facturé sur les opportunités gagnées ce mois (tout ce qui n est pas récurrent (nouveaux clients ou clients existants))</td>
+		<td><!-- REQ2 -->2. CA sur opportunités gagnées</td><td><?=numberFormat(mysqlQuery($req2)->total)?>  €</td><td>CA HT facturé sur les opportunités gagnées ce mois (tout ce qui n est pas récurrent (nouveaux clients ou clients existants))</td>
 	</tr>
 	<tr class="misc1">
-		<td><!-- REQ3 -->3. CA des devis envoyé mais pas transformé</th><td><?=numberFormat(mysqlQuery($req3)->total)?> € - <a href="/prospection/reporting.php?popup=devis_ip&month=<?=$month?>&year=<?=$year?>"><?=numberFormat(mysqlQuery($req3a)->total)?> devis</a></td><td>Indépendant d'une période (donc a un instant T) : CA des devis “Sent” mais pas transformés en Facture</td>
+		<td><!-- REQ3 -->3. CA des devis envoyé mais pas transformé</td><td><?=numberFormat(mysqlQuery($req3)->total)?> € - <a href="/prospection/reporting.php?popup=devis_ip&month=<?=$month?>&year=<?=$year?>"><?=numberFormat(mysqlQuery($req3a)->total)?> devis</a></td><td>Indépendant d'une période (donc a un instant T) : CA des devis “Sent” mais pas transformés en Facture</td>
 	</tr>
 	<tr class="misc2">
-		<td><!-- REQ4 -->4. CA annuel de l'année</th><td><?=numberFormat(mysqlQuery($req4)->total)?> € <br /><?=numberFormat($recu4)?> €, <?=$perc_recu4?>% récurrent <br /> <?=numberFormat($total4_ponctu)?> €, <?=$perc_ponctu4?>% ponctuel</td><td></td>
+		<td><!-- REQ4 -->4. CA annuel de l'année</td><td><?=numberFormat(mysqlQuery($req4)->total)?> € <br /><?=numberFormat($recu4)?> €, <?=$perc_recu4?>% récurrent <br /> <?=numberFormat($total4_ponctu)?> €, <?=$perc_ponctu4?>% ponctuel</td><td></td>
 	</tr>
 	<tr class="misc1">
 		<td><!-- REQ5 -->5. CA des nouveaux clients de l'année</a>
 		</th><td><?=numberFormat(mysqlQuery($req5)->total)?> € - <a href="/prospection/reporting.php?popup=new_client&month=<?=$month?>&year=<?=$year?>"><?=numberFormat(mysqlQuery($req5b)->total)?> nouveaux clients</a> <br /> <?=numberFormat($recu5)?> €, <?=$perc_recu5?>% récurrent <br /> <?=numberFormat($total5_ponctu)?> €, <?=$perc_ponctu5?>% ponctuel</td><td></td>
 	</tr>
+	<tr class="misc2">
+		<td><!-- REQ6 -->6. Nombre de serveurs en infogérance avec GTR</td><td>
+				<table width="300" border="0" cellspacing="0" cellpadding="0">
+					<tr><td width="150">Total</td><td width="50"><?=$total6?></td><td width="100">100%</td></tr>
+					<tr><td>GTR +4 HO</td><td><?=$total6_gtr?></td><td><?=$total6_gtr_per?>%</td></tr>
+					<tr><td>GTR +4 HNO Gold</td><td><?=$total6_gtr4g?></td><td><?=$total6_gtr4g_per?>%</td></tr>
+					<tr><td>GTR +2 HNO Premium</td><td><?=$total6_gtrprem?></td><td><?=$total6_gtrprem_perc?>%</td></tr>
+					<tr><td>GTR +1 HNO</td><td><?=$total6_plus1?></td><td><?=$total6_plus1_perc?>%</td></tr>
+				</table>
+		</td><td></td>
+	</tr>
 </table>
-<? if(isset($_GET['req'])) { ?>
-<pre>
-REQ1
-<?=$req1?>
-
-REQ2
-<?=$req2?>
-
-REQ3
-<?=$req3?>
-
-<?=$req3a?>
-
-<?=$req3b?>
-
-<?=$req3c?>
-
-REQ4
-<?=$req4?>
-
-<?=$req4a?>
-
-REQ5
-<?=$req5?>
-
-<?=$req5a?>
-
-<?=$req5b?>
-
-<?=$req5c?>
-</pre>
-<? } ?>
 <br/>
 <br/>
 <?
