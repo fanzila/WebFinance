82c3fde63be7ec3856965ff54087b305dbd32861
Cyril Bouthors <cyril.bouthors@isvtec.com>
2013-02-26 14:17:14 +0100

    * htdocs/direct_debit/cfonb.php: clean boilerplate

--- htdocs/direct_debit/cfonb.php
+++ htdocs/direct_debit/cfonb.php
@@ -2,6 +2,8 @@
 /*
 This file is part of Webfinance.
 
+Copyright (c) Pierre Doleans <pierre@doleans.net>
+
 Webfinance is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
@@ -16,15 +18,6 @@ You should have received a copy of the GNU General Public License
 along with Webfinance; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-//
-// This file is part of «Webfinance»
-//
-// Copyright (c) 2012 Pierre Doleans
-// Author : Pierre Doleans <pierre@doleans.net>
-//
-// You can use and redistribute this file under the term of the GNU GPL v2.0
-//
 
 include("../inc/main.php");
 must_login();
