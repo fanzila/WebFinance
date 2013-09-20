3ca93a5b7d3206e1b02123f63fabb8a4495e6dc1
Pierre Doleans <pierre@doleans.net>
2013-09-20 11:07:05 -0100

    Replace special chars in sepa lib

--- lib/sepa.php
+++ lib/sepa.php
@@ -124,10 +124,11 @@ function GenerateSepa($debit_id = null) {
 		$montant_centimes = round($info->total_ttc * 100, 0);
 
 		// On enlève les caractères génants
-		$iban = str_replace(",", "", $info->iban);
-		$bic = str_replace(",", "", $info->bic);
+		$iban 			= str_replace(",", "", $info->iban);
+		$bic 			= str_replace(",", "", $info->bic);
 		$client_iban	= preg_replace('/\s+/', '', $iban);
 		$client_bic		= preg_replace('/\s+/', '', $bic);
+		$client_name 	= normalize_special_characters($info->nom_client);
 		
 		// On recherche les éléments pouvant bloquer la génération du paiement
 		
@@ -148,7 +149,8 @@ function GenerateSepa($debit_id = null) {
 			$erreurs_details .= " - Le code BIC de \"".$info->nom_client."\" permettant de générer un virement d'un montant de ".$montant." EUR contient ".strlen($client_bic)." caractères au lieu de ".LONGUEUR_BIC." max. (valeur constatée : ".$client_bic."). Cette ligne est abandonnée.<br />";
 		}
 		
-		$payment = array("name" => $info->nom_client,
+		$payment = array(
+		"name" => $client_name,
 		"IBAN" => $client_iban,
 		"BIC" => $client_bic,
 		"amount" => $montant_centimes,
@@ -203,6 +205,36 @@ function GenerateSepa($debit_id = null) {
 
 	//header("Content-type: text/xml; charset=utf-8");
 	//echo $chaine_totale;
+}
+
+function normalize_special_characters( $str )
+{
+    # Quotes cleanup
+    $str = ereg_replace( chr(ord("`")), "'", $str );        # `
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+    $str = ereg_replace( chr(ord("„")), ",", $str );        # „
+    $str = ereg_replace( chr(ord("`")), "'", $str );        # `
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+    $str = ereg_replace( chr(ord("“")), "\"", $str );        # “
+    $str = ereg_replace( chr(ord("”")), "\"", $str );        # ”
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+
+$unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+$str = strtr( $str, $unwanted_array );
+
+# Bullets, dashes, and trademarks
+$str = ereg_replace( chr(149), "&#8226;", $str );    # bullet •
+$str = ereg_replace( chr(150), "&ndash;", $str );    # en dash
+$str = ereg_replace( chr(151), "&mdash;", $str );    # em dash
+$str = ereg_replace( chr(153), "&#8482;", $str );    # trademark
+$str = ereg_replace( chr(169), "&copy;", $str );    # copyright mark
+$str = ereg_replace( chr(174), "&reg;", $str );        # registration mark
+
+    return $str;
 }
 
 function GetCompanyMaiRIB() {
